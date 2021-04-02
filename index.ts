94d7b80751949906f5016554cd796b4f12331281
OnlyLlI <2602280163@qq.com>
2021-04-02 11:35:29 +0800

    第一个ts项目

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
 let a : {name : string,age ?: number};
+let b : {name : string, age : number};
 a = {name : '孙悟空',age : 19}
+b = {name : '猪八戒',age : 18}
 console.log(a)
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 "use strict";
 var a;
+var b;
 a = { name: '孙悟空', age: 19 };
+b = { name: '猪八戒', age:  };
 console.log(a);
